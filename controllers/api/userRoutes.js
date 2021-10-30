fd095490e7a9df4cc5bdacb31b47d2f1eb86eeed
Brent Graves <graves.brent@gmail.com>
2021-10-30 15:15:53 -0400

    added route to create user profile.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,3 +22,21 @@ router.post("/login", async (req, res) => {
         res.status(500).json(err);
     }
 });
+// route for creating a a profile user
+router.post("/", async (req, res) => {
+    try {
+        const { email, userName, password } = req.body;
+
+        if (!email || !userName || !password ) res.status(400).send("You must add a username, email and password to continue");
+
+        const userData = await User.create({ email, userName, password });
+
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
