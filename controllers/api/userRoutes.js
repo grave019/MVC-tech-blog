de16c9665e75dc8518a086b8981eaaa15df1141f
Brent Graves <graves.brent@gmail.com>
2021-10-30 15:14:11 -0400

    added route for login session.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,24 @@
 const router = require("express").Router();
 const { User } = require("../../models");
+//route to create a user login session
+router.post("/login", async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+        if (!userData) {
+            res.status(400).json({ message: "Username and Password is needed" });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
