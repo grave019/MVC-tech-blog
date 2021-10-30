958a1833c302a6dad72407470db287a9363fd508
Brent Graves <graves.brent@gmail.com>
2021-10-30 14:32:21 -0400

    added async route in homeroutes to render the homepage.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,24 @@
 const router = require("express").Router();
 const { Post, User } = require("../models");
+// route to render the homepage
+router.get("/", async (req, res) => {
+    try {
+        const blogData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["userName"]
+                }
+            ]
+        });
+        const posts = blogData.map((post) => post.get({ plain: true }));
+
+        res.render("homepage", {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
