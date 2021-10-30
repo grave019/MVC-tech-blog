9a6e1dbdbee6aa76586942639f7c45874c988fec
Brent Graves <graves.brent@gmail.com>
2021-10-30 15:23:42 -0400

    added post.init to sequalize post.js

--- models/Post.js
+++ models/Post.js
@@ -1,2 +1,39 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+// sequelize Post model
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        content: {
+            type: DataTypes.TEXT,
+        },
+        dateCreated: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "users",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: "post",
+    }
+);
+
+module.exports = Post;
