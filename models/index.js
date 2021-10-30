df36e055b504730098337fb7a83ca7643a4026fc
Brent Graves <graves.brent@gmail.com>
2021-10-30 02:26:37 -0400

    added models folder with js files and initial requirements.

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require("./User");
+const Post = require("./Post");
+
+User.hasMany(Post, {
+    foreignKey: "userId",
+    onDelete: "CASCADE"
+});
+
+Post.belongsTo(User, {
+    foreignKey: "userId"
+});
+
+module.exports = { User, Post };
